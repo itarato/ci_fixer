d4ab17f5540ec3273520d4e04e9fcdec8d614f4e
Peter Arato <it.arato@gmail.com>
2023-08-27 19:09:26 -0400

    Tidy up.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
-mod reader;
 mod ci_results;
-mod util;
+mod reader;
 mod runner;
+mod util;
 
 use crate::reader::*;
 use crate::runner::*;
 
 fn main() {
-    let _result = InputBlobReader{}
+    let _result = InputBlobReader {}
         .read()
-        .and_then(|ci_results|  {
-            Runner::new("/home/itarato/Desktop/testserver/testmri", ci_results)
-        })
-        .map(|mut runner| {
-            runner.run();
+        .and_then(|ci_results| Runner::new("/home/itarato/Desktop/testserver/testmri", ci_results))
+        .and_then(|mut runner| runner.run())
+        .unwrap_or_else(|err| {
+            eprintln!("Execution has failed with: {}", err);
         });
 }
